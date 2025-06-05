8798f073c2f080ab44cfbb98f49a65f8236fbc4f
S_S_D <swsd6894@gmail.com>
2025-06-05 12:14:13 +0100

    added video optimization

--- src/constants/index.js
+++ src/constants/index.js
@@ -6,7 +6,11 @@ import {
   smallRolexFirstVideo,
   smallRolexSecondVideo,
   smallRolexThirdVideo,
-  smallRolexFourthVideo
+  smallRolexFourthVideo,
+  videoThumbnailOne,
+  videoThumbnailTwo,
+  videoThumbnailThree,
+  videoThumbnailFour
 } from "../utils";
 
 export const navLists = ["Rolex Watches", "Store Locator", "Support", "About Rolex"];
@@ -16,25 +20,29 @@ export const videoSlides = [
     id: 1,
     descriptionList: ["The evolution of a tool watch."],
     videoSrc: rolexFirstVideo,
-    videoDuration: 5
+    videoDuration: 5,
+    thumbnail: videoThumbnailOne
   },
   {
     id: 2,
     descriptionList: ["A tool watch in step with the times."],
     videoSrc: rolexSecondVideo,
-    videoDuration: 6
+    videoDuration: 6,
+    thumbnail: videoThumbnailTwo
   },
   {
     id: 3,
     descriptionList: ["A pioneering divers’ watch."],
     videoSrc: rolexThirdVideo,
-    videoDuration: 11
+    videoDuration: 11,
+    thumbnail: videoThumbnailThree
   },
   {
     id: 4,
     descriptionList: ["Waterproofness:", "a founding quest,", "a perpetual challenge."],
     videoSrc: rolexFourthVideo,
-    videoDuration: 9
+    videoDuration: 9,
+    thumbnail: videoThumbnailFour
   }
 ];
 
@@ -43,25 +51,29 @@ export const smallVideoSlides = [
     id: 1,
     descriptionList: ["The evolution of a tool watch."],
     videoSrc: smallRolexFirstVideo,
-    videoDuration: 5
+    videoDuration: 5,
+    thumbnail: videoThumbnailOne
   },
   {
     id: 2,
     descriptionList: ["A tool watch in step with the times."],
     videoSrc: smallRolexSecondVideo,
-    videoDuration: 6
+    videoDuration: 6,
+    thumbnail: videoThumbnailTwo
   },
   {
     id: 3,
     descriptionList: ["A pioneering divers’ watch."],
     videoSrc: smallRolexThirdVideo,
-    videoDuration: 11
+    videoDuration: 11,
+    thumbnail: videoThumbnailThree
   },
   {
     id: 4,
     descriptionList: ["Waterproofness:", "a founding quest,", "a perpetual challenge."],
     videoSrc: smallRolexFourthVideo,
-    videoDuration: 9
+    videoDuration: 9,
+    thumbnail: videoThumbnailFour
   }
 ];
 
