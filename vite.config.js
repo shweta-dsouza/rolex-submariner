50417927cec420ca7eeae73d66f06ade46e9bb03
S_S_D <swsd6894@gmail.com>
2025-06-18 09:09:47 +0100

    added lazy and dynamic imports

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,28 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { compression } from 'vite-plugin-compression2';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    compression({
+      algorithms: ['brotliCompress'],
+      deleteOriginalAssets: true
+    })
+  ],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor'; // Split vendor libraries
+          }
+          if (id.includes('src/components/')) {
+            return 'components'; // Split components into their own chunk
+          }
+        },
+      },
+    },
+  }
 })
